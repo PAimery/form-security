e4db8d0cb578f8b3f2108b4683fd0df1fc646dc8
Aimery Pineau <aimery.pin@gmail.com>
2023-03-31 15:13:29 +0200

    formulaire

--- form.php
+++ form.php
@@ -1,3 +1,29 @@
+<?php
+
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+  if (!isset($_POST['user_lname']) || trim($_POST['user_lname']) === '')
+    $errors[] = "Le nom est obligatoire";
+  if (!isset($_POST['user_fname']) || trim($_POST['user_fname']) === '')
+    $errors[] = "Le prénom est obligatoire";
+  if (!isset($_POST['email']) || trim($_POST['email']) === '')
+    $errors[] = "L'email est obligatoire";
+
+  if (!isset($_POST['phone']) || trim($_POST['phone']) === '')
+    $errors[] = "Le numéro de téléphone est obligatoire";
+  if (!isset($_POST['gender']) || trim($_POST['gender']) === '')
+    $errors[] = "Le nom est obligatoire";
+  if (!isset($_POST['message']) || trim($_POST['message']) === '')
+    $errors[] = "Le champ message est obligatoire";
+
+  if (empty($errors)) {
+    header('Location: thanks.php');
+  }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,41 +35,50 @@
 </head>
 
 <body>
+  <main class="container">
+    <h1>Inscriptions</h1>
+    <?php if (count($errors) > 0) : ?>
+      <ul>
+        <?php foreach ($errors as $error) : ?>
+          <li><?= $error ?></li>
+        <?php endforeach; ?>
+      </ul>
+    <?php endif; ?>
 
-
-  <form action="thanks.php" method="post">
-    <div>
-      <label for="lname">Nom :</label>
-      <input type="text" id="lname" name="user_lname" placeholder="Lastname">
-    </div>
-    <div>
-      <label for="fname">Prénom :</label>
-      <input type="text" id="name" name="user_fname" placeholder="Firstname">
-    </div>
-    <div>
-      <label for="courriel">Courriel :</label>
-      <input type="email" id="courriel" name="email" placeholder="Email">
-    </div>
-    <div>
-      <label for="phone">Téléphone :</label>
-      <input type="tel" id="phone" name="phone" placeholder="Phone">
-    </div>
-    <div>
-      <label for="gender">Sexe :</label>
-      <select name="gender" id="gender" name="gender">
-        <option value="man">Homme</option>
-        <option value="woman">Femme</option>
-        <option value="other">Autre</option>
-      </select>
-    </div>
-    <div>
-      <label for="message">Message :</label>
-      <textarea id="message" name="message" placeholder="Message"></textarea>
-    </div>
-    <div class="button">
-      <button type="submit">Envoyer votre message</button>
-    </div>
-  </form>
+    <form action="thanks.php" method="post">
+      <div>
+        <label for="lname">Nom :</label>
+        <input type="text" id="lname" name="user_lname" placeholder="Lastname" required>
+      </div>
+      <div>
+        <label for="fname">Prénom :</label>
+        <input type="text" id="fname" name="user_fname" placeholder="Firstname" required>
+      </div>
+      <div>
+        <label for="courriel">Courriel :</label>
+        <input type="email" id="courriel" name="email" placeholder="Email" required>
+      </div>
+      <div>
+        <label for="phone">Téléphone :</label>
+        <input type="tel" id="phone" name="phone" placeholder="Phone" required>
+      </div>
+      <div>
+        <label for="gender">Sexe :</label>
+        <select name="gender" id="gender" name="gender" required>
+          <option value="man">Homme</option>
+          <option value="woman">Femme</option>
+          <option value="other">Autre</option>
+        </select>
+      </div>
+      <div>
+        <label for="message">Message :</label>
+        <textarea id="message" name="message" placeholder="Message" required></textarea>
+      </div>
+      <div class="button">
+        <button type="submit">Envoyer votre message</button>
+      </div>
+    </form>
+  </main>
 </body>
 
 </html>
